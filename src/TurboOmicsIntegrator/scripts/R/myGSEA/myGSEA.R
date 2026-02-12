761ff24873e15c7880d37f7a5305a24ea195d4c9
jmrodriguezc <jmrodriguezc1977@gmail.com>
2026-02-12 07:08:42 +0100

    Fixing problems with the new R version

--- src/TurboOmicsIntegrator/scripts/R/myGSEA/myGSEA.R
+++ src/TurboOmicsIntegrator/scripts/R/myGSEA/myGSEA.R
@@ -1,151 +1,228 @@
-# if (!require("Biocmanager", quietly = true))
-#   install.packages("BiocManager")
-# 
-# install.packages("jsonlite")
-# BiocManager::install("fgsea")
-# BiocManager::install("reactome.db")
-# 
-
 cat('** Start script: myGSEA.R\n')
 
 # Constants
-DATE_DB <- "202311" # date of the database employed
+DATE_DB <- "202311"
 
 # Get command line arguments
-args = commandArgs(trailingOnly = T)
+args = commandArgs(trailingOnly = TRUE)
 
-basePath = args[1] # "S:\\U_Proteomica\\UNIDAD\\Softwares\\MacrosRafa\\data\\Metabolomics\\PESA_Integromics\\TestServer\\GSEA\\jobs\\Mus_musculus-UniprotID-Treatment_A12_PBS"  #args[0]
-workingPath = args[2] # path to job_id
-org = args[3] # "Mus_musculus" #args[1]
+basePath    <- args[1]
+workingPath <- args[2]
+org         <- args[3]
 
-# Import libraries
+# Libraries
 library(fgsea)
 library(reactome.db)
 library(jsonlite)
 
 
-# Open json with rank info
+# -----------------------------
+# Load data
+# -----------------------------
 
 cat('**\n')
-cat("** Working Path: ", workingPath)
-cat('\n**\n')
+cat("** Working Path: ", workingPath, "\n")
+cat('**\n')
 
-df <- as.data.frame(fromJSON(paste0(workingPath, '/EID_GN_RankStat.json')))
-df <- df[order(df$RankStat, decreasing = T),]
+df <- as.data.frame(fromJSON(
+  paste0(workingPath, '/EID_GN_RankStat.json')
+))
 
-# Create vector with statistic
-rankStat <- df$RankStat
 
-# Enrichment with HALLMARK
+# -----------------------------
+# Prepare GENE SYMBOL ranking
+# -----------------------------
+
+# Uppercase
+df$GeneName <- toupper(df$GeneName)
+
+# Remove NA
+df_gene <- df[!is.na(df$GeneName), ]
+
+# Collapse duplicates (max abs value)
+df_gene_unique <- aggregate(
+  RankStat ~ GeneName,
+  data = df_gene,
+  FUN = function(x) x[which.max(abs(x))]
+)
+
+# Sort
+df_gene_unique <- df_gene_unique[
+  order(df_gene_unique$RankStat, decreasing = TRUE),
+]
+
+# Create ranked vector
+rankStat_gene <- df_gene_unique$RankStat
+names(rankStat_gene) <- df_gene_unique$GeneName
+
+
+# Safety check
+stopifnot(!any(duplicated(names(rankStat_gene))))
+
+
+# -----------------------------
+# Prepare ENTREZ ranking (Reactome)
+# -----------------------------
+
+df$EntrezGene <- as.character(df$EntrezGene)
+
+df_entrez <- df[!is.na(df$EntrezGene), ]
+
+df_entrez_unique <- aggregate(
+  RankStat ~ EntrezGene,
+  data = df_entrez,
+  FUN = function(x) x[which.max(abs(x))]
+)
+
+df_entrez_unique <- df_entrez_unique[
+  order(df_entrez_unique$RankStat, decreasing = TRUE),
+]
+
+rankStat_entrez <- df_entrez_unique$RankStat
+names(rankStat_entrez) <- df_entrez_unique$EntrezGene
+
+
+stopifnot(!any(duplicated(names(rankStat_entrez))))
+
+
+# -----------------------------
+# HALLMARK
+# -----------------------------
+
 cat('** Perform enrichment with HALLMARK\n')
 
-names(rankStat) <- toupper(df$GeneName)
+rankStat <- rankStat_gene
 
 pathways <- gmtPathways(
-  paste0(basePath, "/mydb/", DATE_DB, "/Homo_sapiens/", "h.all.v2023.2.Hs.symbols.gmt")
-  )
+  paste0(basePath, "/mydb/", DATE_DB,
+         "/Homo_sapiens/h.all.v2023.2.Hs.symbols.gmt")
+)
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$pval < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/HALLMARK_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/HALLMARK_GSEA.json")
+)
 
-# Enrichment with GO:MF
+
+# -----------------------------
+# GO:MF
+# -----------------------------
+
 cat('** Perform enrichment with GO:MF\n')
 
-names(rankStat) <- toupper(df$GeneName)
+rankStat <- rankStat_gene
 
 pathways <- gmtPathways(
-  paste0(basePath, "/mydb/", DATE_DB, "/Homo_sapiens/", "c5.go.mf.v2023.2.Hs.symbols.gmt")
-  )
+  paste0(basePath, "/mydb/", DATE_DB,
+         "/Homo_sapiens/c5.go.mf.v2023.2.Hs.symbols.gmt")
+)
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$padj < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/GO_MF_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/GO_MF_GSEA.json")
+)
 
-# Enrichment with GO:CC
+
+# -----------------------------
+# GO:CC
+# -----------------------------
+
 cat('** Perform enrichment with GO:CC\n')
 
-names(rankStat) <- toupper(df$GeneName)
+rankStat <- rankStat_gene
 
 pathways <- gmtPathways(
-  paste0(basePath, "/mydb/", DATE_DB, "/Homo_sapiens/", "c5.go.cc.v2023.2.Hs.symbols.gmt")
-  )
+  paste0(basePath, "/mydb/", DATE_DB,
+         "/Homo_sapiens/c5.go.cc.v2023.2.Hs.symbols.gmt")
+)
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$padj < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/GO_CC_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/GO_CC_GSEA.json")
+)
 
-# Enrichment with GO:BP
+
+# -----------------------------
+# GO:BP
+# -----------------------------
+
 cat('** Perform enrichment with BP\n')
 
-names(rankStat) <- toupper(df$GeneName)
+rankStat <- rankStat_gene
 
 pathways <- gmtPathways(
-  paste0(basePath, "/mydb/", DATE_DB, "/Homo_sapiens/", "c5.go.bp.v2023.2.Hs.symbols.gmt")
-  )
+  paste0(basePath, "/mydb/", DATE_DB,
+         "/Homo_sapiens/c5.go.bp.v2023.2.Hs.symbols.gmt")
+)
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$padj < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/GO_BP_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/GO_BP_GSEA.json")
+)
 
-# Enrichment with Reactome
+
+# -----------------------------
+# Reactome
+# -----------------------------
+
 cat('** Perform enrichment with Reactome\n')
 
-names(rankStat) <- df$EntrezGene
+rankStat <- rankStat_entrez
 
 pathways <- reactomePathways(names(rankStat))
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$padj < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/REACTOME_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
-
-# Enrichment with KEGG
-cat("** Perform enrichment with KEGG\n")
-
-names(rankStat) <- df$GeneName
-
-pathways <- gmtPathways(
-  paste0(basePath, "/mydb/", DATE_DB, "/", org,  "/", "kegg.gmt")
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/REACTOME_GSEA.json")
 )
 
-fgseaRes <- fgsea(pathways, rankStat, maxSize=500, minSize=5)
+
+# -----------------------------
+# KEGG
+# -----------------------------
+
+cat("** Perform enrichment with KEGG\n")
+
+rankStat <- rankStat_gene
+
+pathways <- gmtPathways(
+  paste0(basePath, "/mydb/", DATE_DB, "/", org, "/kegg.gmt")
+)
+
+fgseaRes <- fgsea(pathways, rankStat, maxSize = 500, minSize = 5)
 
 out <- fgseaRes[fgseaRes$padj < 0.1]
 out <- out[order(out$pval)]
 
-outJson <- toJSON(out)
-fileConn<-file(paste0(workingPath, "/KEGG_GSEA.json"))
-writeLines(outJson, fileConn)
-close(fileConn)
+writeLines(
+  toJSON(out),
+  paste0(workingPath, "/KEGG_GSEA.json")
+)
+
 
 cat('** End script\n')
